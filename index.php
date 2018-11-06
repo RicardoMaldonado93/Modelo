edfe78ad193ab7bd3b247f21b7b7a33cb8788006
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2018-11-06 01:52:34 -0300

    'PDO'

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once './vendor/autoload.php';
+require_once './api/CDApi.php';
+
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$app = new \Slim\App(["settings" => $config]);
+
+
+$app->group('/cds', function(){
+
+    $this->get('/', function($response){ 
+        $response->getBody()->write("asdsd"); 
+        return $response;}
+    );
+    $this->get('/traerTodos', \CDApi::class . ':TraerTodos');
+
+    $this->get('/traerUnCD/{id}', \CDApi::class . ':TraerUnCD');
+
+    $this->post('/agregarUnCD', \CDApi::class . ':AgregarUnCD');
+
+    $this->delete('/borrarUnCD/{id}', \CDApi::class . ':BorrarUnCD');
+    
+    $this->put('/modificarUnCD/{id}', \CDApi::class . ':ModificarUnCD');
+});
+
+$app->run();
+?>
